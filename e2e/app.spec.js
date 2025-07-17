dd18afee2d8bbfa3bb34e05873150f81e66bc811
yassineRiahi <171589312+RiahiYassine@users.noreply.github.com>
2025-07-17 11:57:02 +0100

    change website content

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,13 +3,13 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/Learn Jenkins/);
+  await expect(page).toHaveTitle(/Yassine Riahi/);
 });
 
-test('has Jenkins in the body', async ({ page }) => {
+test('has LinkedIn link in the body', async ({ page }) => {
   await page.goto('/');
 
-  const isVisible = await page.locator('a:has-text("Learn Jenkins on Udemy")').isVisible();
+  const isVisible = await page.locator('a:has-text("Connect with me on LinkedIn")').isVisible();
   expect(isVisible).toBeTruthy();
 });
 
@@ -17,8 +17,6 @@ test('has expected app version', async ({ page }) => {
   await page.goto('/');
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
-
-  console.log(expectedAppVersion);
 
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
   expect(isVisible).toBeTruthy();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const version = process.env.REACT_APP_VERSION || '1';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Jenkins on Udemy
-        </a>
-      </header>
-      <p>
-          Application version: 1
-      </p>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, I'm Yassine Riahi</h1>
+          <p>
+            I'm a full-stack software engineer with hands-on experience in Java and Angular. I’ve worked extensively with GitHub Actions for CI/CD, and I'm currently expanding my DevOps skills by exploring Jenkins. The idea behind this transition is to externalize the CI/CD process for greater flexibility, control, and integration across different environments and tools.
+          </p>
+          <p>
+            This site is part of my personal DevOps workflow — fully automated with Jenkins and Playwright end-to-end tests, and deployed through Netlify.
+          </p>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/yassine-riahi-012307255/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Connect with me on LinkedIn
+          </a>
+        </header>
+      </div>
+      <footer className="App-footer">
+        <p className="version">Application version: {version}</p>
+      </footer>
+    </>
+
   );
 }
 
