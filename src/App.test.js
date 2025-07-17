f72c202f3d5cdf216ed13a714744c1fe7ec59125
yassineRiahi <171589312+RiahiYassine@users.noreply.github.com>
2025-07-17 12:03:34 +0100

    modify  test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn Jenkins link', () => {
+test('renders LinkedIn connection link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Jenkins/i);
+  const linkElement = screen.getByText(/connect with me on linkedin/i);
   expect(linkElement).toBeInTheDocument();
 });
